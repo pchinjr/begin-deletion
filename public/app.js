370c27937b3565b7e7a6499e159bbd14cd481298
Paul Chin Jr <pchinjr@gmail.com>
2020-08-31 10:36:27 -0400

    updated app

--- public/app.js
+++ public/app.js
@@ -1,5 +1,5 @@
 var characters = []; //empty array to hold the split characters
- 
+
 $('#content').on('keyup', function(){
   var entry = $("#content").val();
   var splitt = entry.split("");
@@ -17,21 +17,3 @@ var removeTimer = $.timer( function() {
 }, 3000, false);
 
 autosize(document.querySelectorAll('textarea'));
-
-// form action
-/*$("form").submit( function (event) {
-  var entry = $("#input").val(); //grabs the text in the input box
-  var charCount = $("#input").val().split(''); //splits the input into an array of characters
-  var emptySpace = [" "]; //array to hold an empty space
-  var charTotal = charCount.length; //length of array 
-  
-  Array.prototype.push.apply(characters, charCount); //puts the input array into the empty array
-  Array.prototype.push.apply(characters, emptySpace); //adds a space to the characters array
-  
-  event.preventDefault();
-  $("#input").val(" "); //clear input box
-  $('#content').html( '<h3>' + characters.join("") + '<span class="typed-cursor">|</span></h3>' );
-  removeTimer.play();
-});*/
-
-
